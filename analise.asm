d423a1f1a9fd5f490cc7e3728c29e580c92c29e7
Tadeu Gomes <jtgsf@cesar.school>
2025-11-27 15:09:44 -0300

    atualização do codigo

--- analise.asm
+++ analise.asm
@@ -2,66 +2,52 @@
 .globl validar_entrada
 .globl calcular_forca
 
-# ------------------------------------------------------------------
-# SUB-ROTINA: validar_entrada
-# Verifica se a string tem caracteres uteis (não é só \n ou nula)
-# Retorna $v0 = 1 (OK) ou 0 (Erro)
-# ------------------------------------------------------------------
 validar_entrada:
-    lb $t0, 0($a0)               # Lê primeiro char
-    beq $t0, 10, erro_validacao  # Se for \n (ASCII 10)
-    beq $t0, 0, erro_validacao   # Se for Null
+    lb $t0, 0($a0)
+    beq $t0, 10, erro_validacao
+    beq $t0, 0, erro_validacao
     
-    li $v0, 1           # Sucesso
+    li $v0, 1
     jr $ra
 
 erro_validacao:
-    li $v0, 0           # Falha
+    li $v0, 0
     jr $ra
 
-# ------------------------------------------------------------------
-# SUB-ROTINA: calcular_forca
-# Analisa complexidade. Usa registradores salvos ($s) conforme regra.
-# ------------------------------------------------------------------
 calcular_forca:
-    # PRÓLOGO (Salva contexto)
     addi $sp, $sp, -16
     sw $ra, 12($sp)
-    sw $s0, 8($sp)      # Endereço string
-    sw $s1, 4($sp)      # Score acumulado
-    sw $s2, 0($sp)      # Tamanho
+    sw $s0, 8($sp)
+    sw $s1, 4($sp)
+    sw $s2, 0($sp)
 
-    move $s0, $a0       # $s0 recebe argumento
-    li $s1, 0           # Score inicial = 0
-    li $s2, 0           # Tamanho = 0
+    move $s0, $a0
+    li $s1, 0
+    li $s2, 0
 
-    # Flags temporárias ($t0=Upper, $t1=Lower, $t2=Special)
     li $t0, 0
     li $t1, 0
     li $t2, 0
 
 loop_analise:
-    lb $t3, 0($s0)          # Lê char
-    beq $t3, 0, fim_loop    # Null terminator
-    beq $t3, 10, fim_loop   # Newline
+    lb $t3, 0($s0)
+    beq $t3, 0, fim_loop
+    beq $t3, 10, fim_loop
 
-    addi $s2, $s2, 1        # Incrementa tamanho
+    addi $s2, $s2, 1
     
-    # Verifica Maiuscula (65-90)
     blt $t3, 65, check_min
     bgt $t3, 90, check_min
-    li $t0, 1               # Flag Upper OK
+    li $t0, 1
     j next_char
 
 check_min:
-    # Verifica Minuscula (97-122)
     blt $t3, 97, check_spec
     bgt $t3, 122, check_spec
-    li $t1, 1               # Flag Lower OK
+    li $t1, 1
     j next_char
 
 check_spec:
-    # Se nao é letra, considera especial/numero
     li $t2, 1
 
 next_char:
@@ -69,26 +55,23 @@ next_char:
     j loop_analise
 
 fim_loop:
-    # SOMATÓRIO DOS PONTOS
-    # Se tamanho >= 8, +1 ponto
     bge $s2, 8, ganha_ponto_tam
     j soma_flags
+
 ganha_ponto_tam:
-    addi $s1, $s1, 2        # Tamanho vale 2 pontos neste modelo
+    addi $s1, $s1, 2
 
 soma_flags:
-    add $s1, $s1, $t0       # +1 se teve Maiuscula
-    add $s1, $s1, $t1       # +1 se teve Minuscula
-    add $s1, $s1, $t2       # +1 se teve Especial
+    add $s1, $s1, $t0
+    add $s1, $s1, $t1
+    add $s1, $s1, $t2
 
-    # Limita score maximo a 5 (opcional)
     ble $s1, 5, return_score
     li $s1, 5
 
 return_score:
-    move $v0, $s1           # Retorno em $v0
+    move $v0, $s1
 
-    # EPÍLOGO (Restaura contexto)
     lw $s2, 0($sp)
     lw $s1, 4($sp)
     lw $s0, 8($sp)
--- display.asm
+++ display.asm
@@ -1,21 +1,15 @@
 .data
-    str_bar:  .asciiz "#"
-    str_dot:  .asciiz "."
-    str_l:    .asciiz "["
-    str_r:    .asciiz "]"
+    str_bar: .asciiz "#"
+    str_dot: .asciiz "."
+    str_l:   .asciiz "["
+    str_r:   .asciiz "]"
 
 .text
-# ------------------------------------------------------------------
-# SUB-ROTINA: desenhar_status
-# Entrada: $a0 (score 0-5)
-# Não retorna valor.
-# ------------------------------------------------------------------
 desenhar_status:
-    move $t0, $a0        # $t0 = Pontos (preenchidos)
-    li $t1, 5            
-    sub $t1, $t1, $t0    # $t1 = Vazios (5 - pontos)
+    move $t0, $a0
+    li $t1, 5
+    sub $t1, $t1, $t0
 
-    # Imprime '['
     li $v0, 4
     la $a0, str_l
     syscall
@@ -23,7 +17,7 @@ desenhar_status:
 loop_preenchido:
     blez $t0, loop_vazios
     li $v0, 4
-    la $a0, str_bar      # Imprime "##"
+    la $a0, str_bar
     syscall
     addi $t0, $t0, -1
     j loop_preenchido
@@ -31,18 +25,16 @@ loop_preenchido:
 loop_vazios:
     blez $t1, fim_desenho
     li $v0, 4
-    la $a0, str_dot      # Imprime ".."
+    la $a0, str_dot
     syscall
     addi $t1, $t1, -1
     j loop_vazios
 
 fim_desenho:
-    # Imprime ']'
     li $v0, 4
     la $a0, str_r
     syscall
     
-    # Imprime quebra de linha
     li $v0, 11
     li $a0, 10
     syscall
